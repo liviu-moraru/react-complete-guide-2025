453a7b5242a92a477bd23226a90f985b82fdb82d
liviu-moraru <liviu.moraru@gmail.com>
2026-01-15 16:34:46 +0200

    05 Essentials Practice - 05-outputting results-in-a-list
    Update `Results` to render computed table; integrate formatted investment data with yearly details

--- 05-Essentials-Practice/react-essentials-practice/src/components/Results.jsx
+++ 05-Essentials-Practice/react-essentials-practice/src/components/Results.jsx
@@ -1,8 +1,38 @@
-import { calculateInvestmentResults } from "../util/investment.js";
+import { calculateInvestmentResults, formatter } from "../util/investment.js";
 
 export default function Results({ input }) {
-  console.log(input);
+  //console.log(input);
   const result = calculateInvestmentResults(input);
   console.log(result);
-  return <p>Results...</p>;
+  let totalInterest = 0;
+  let investedCapital = input.initialInvestment;
+
+  return (
+    <table id="result">
+      <thead>
+        <tr>
+          <th>Year</th>
+          <th>Investment Value</th>
+          <th>Interest (Year)</th>
+          <th>Total interest</th>
+          <th>Invested Capital</th>
+        </tr>
+      </thead>
+      <tbody>
+        {result.map((yearData, index) => {
+          totalInterest += yearData.interest;
+          investedCapital += input.annualInvestment;
+          return (
+            <tr key={yearData.year}>
+              <td>{formatter.format(yearData.year)}</td>
+              <td>{formatter.format(yearData.valueEndOfYear)}</td>
+              <td>{formatter.format(yearData.interest)}</td>
+              <td>{formatter.format(totalInterest)}</td>
+              <td>{formatter.format(investedCapital)}</td>
+            </tr>
+          );
+        })}
+      </tbody>
+    </table>
+  );
 }
