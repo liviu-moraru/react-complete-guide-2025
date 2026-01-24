08a98e6c6f29eed0143b73356ce7b99be2d2fd20
liviu-moraru <liviu.moraru@gmail.com>
2026-01-24 17:55:29 +0200

    08 Refs Portals - 04-manipulating-dom-with-refs
    Refactor `Player` component: rename `playerRef` to `playerName` for clarity and reset input field after submission

--- 08-Refs-Portals/refs/src/components/Player.jsx
+++ 08-Refs-Portals/refs/src/components/Player.jsx
@@ -1,22 +1,23 @@
 import { useState, useRef } from "react";
 
 export default function Player() {
-  const playerRef = useRef();
+  const playerName = useRef();
   const [enteredPlayerName, setEnteredPlayerName] = useState(null);
 
   function handleClick() {
-    let inputValue = playerRef.current.value;
+    let inputValue = playerName.current.value;
     if (!inputValue) {
       inputValue = null;
     }
     setEnteredPlayerName(inputValue);
+    playerName.current.value = "";
   }
   console.log("log", enteredPlayerName ?? "unknown entity");
   return (
     <section id="player">
       <h2>Welcome {enteredPlayerName ?? "unknown entity"}</h2>
       <p>
-        <input type="text" ref={playerRef} />
+        <input type="text" ref={playerName} />
         <button onClick={handleClick}>Set Name</button>
       </p>
     </section>
